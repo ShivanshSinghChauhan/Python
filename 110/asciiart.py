b5eee2738f0d3fd7adc036aa0bf9f3fe2eba87e5
ShivanshSinghChauhan <shivanshsinghchauhan@gmail.com>
2018-09-17 15:07:25 -0700

    Add files via upload
    Final Commit

---
+++ 110/asciiart.py
@@ -0,0 +1,34 @@
+# Name- Shivansh Singh Chauhan, CSc 110, Spring 2017, Section- 2I
+# Programming Assignment #2, Part A
+# Date- 27 January, 2017
+
+# This program is made between 3 to 200 lines. This program is an ASCII art.
+# This programs consists of one "for loop" and other prints statements. 
+
+
+
+def main(): # this the main fuction includes all the prints statements and for loop.
+    print("____________")
+    print("|          |")
+    print("|          |")
+    print("|          |")
+    print("|   STOP   |")
+    print("|          |")
+    print("|          |")
+    print("____________")
+    print("    |   |   ")
+    print("    |   |   ")
+    print("    |   |   ")
+    print("    |   |   ")
+    print("    |   |   ")
+    print("    |   |   ")
+    print("    |   |   ")
+    print("    |   |   ")
+    for i in range(1, 13): # this is the only "for loop"
+        print("_", end="")
+    print("THE STOP SIGN")    
+        
+    
+       
+main()    
+    
---
+++ 110/doodle.py
@@ -0,0 +1,24 @@
+# Name- Shivansh Singh Chauhan, CSc 110, Spring 2017, Section- 2I
+# Programming Assignment #2, Part A
+# Date- 3 February, 2017
+
+# This Program contains three shapes, uses at three distinct colors  
+
+from drawingpanel import *
+
+# This is the main fuction contains all the sub-fuctions.
+# Man its easy there is nothing to read.
+# Come on you still here read cafewall there is more insteresting stuff wating fayou  
+
+def main(): 
+             
+    p = DrawingPanel( 300, 200, background="black")
+
+    p.canvas.create_rectangle(50,50,100,100, fill="red", width=2)
+    p.canvas.create_oval(100,50,120,120, fill="yellow", width=2)
+    p.canvas.create_rectangle(150,50,200,200, fill="green", width=2)
+
+           
+        
+main()
+
